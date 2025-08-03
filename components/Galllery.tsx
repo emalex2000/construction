707fa50cb918712bada93e6ac6c41b5cd09a2b38
Emalex <emalexogah1999@gmail.com>
2025-08-03 01:30:22 +0100

    Update Galllery.tsx

--- components/Galllery.tsx
+++ components/Galllery.tsx
@@ -1,9 +1,9 @@
 export default function MyBentoGrid() {
   return (
-    <div className="flex flex-row gap-[1.5rem] mt-7">
+    <div className="flex flex-col md:flex-row gap-[1.5rem] mt-7 px-4 md:px-0">
       {/* Left Column */}
-      <div className="flex flex-col gap-[1.5rem]">
-        <div style={{padding:"0px", margin:"0px"}} className="shadow-sm  border border-gray-300 w-[12rem] h-[24rem] rounded-2xl overflow-hidden p-[0px] m-[0px] relative">
+      <div className="flex flex-col gap-[1.5rem] items-center md:items-start">
+        <div className="shadow-sm border border-gray-300 w-[12rem] h-[24rem] rounded-2xl overflow-hidden">
           <img
             src="https://images.pexels.com/photos/5505125/pexels-photo-5505125.jpeg"
             alt=""
@@ -21,33 +21,31 @@ export default function MyBentoGrid() {
       </div>
 
       {/* Right Column */}
-      <div className="w-[24rem] h-[34.5rem] flex flex-col justify-between gap-5">
-        <div className="h-[24rem] flex flex-col justify-between">
-          <div className="flex flex-col h-full justify-between w-full">
-            <div className="w-full h-[11.5rem] border border-gray-300 rounded-2xl shadow-sm overflow-hidden">
+      <div className="w-full md:w-[24rem] flex flex-col justify-between gap-5">
+        <div className="flex flex-col justify-between gap-[1.5rem]">
+          <div className="w-full h-[11.5rem] border border-gray-300 rounded-2xl shadow-sm overflow-hidden">
+            <img
+              src="https://images.pexels.com/photos/439416/pexels-photo-439416.jpeg"
+              alt=""
+              className="w-full h-full object-cover"
+            />
+          </div>
+
+          <div className="flex flex-col sm:flex-row gap-[1.5rem]">
+            <div className="w-full sm:w-[12rem] h-[9rem] border shadow-sm border-gray-300 rounded-2xl overflow-hidden">
               <img
-                src="https://images.pexels.com/photos/439416/pexels-photo-439416.jpeg"
+                src="https://images.pexels.com/photos/23775788/pexels-photo-23775788.jpeg"
                 alt=""
                 className="w-full h-full object-cover"
               />
             </div>
 
-            <div className="w-full h-[11.5rem] flex flex-row gap-[1.5rem]">
-              <div className="w-[12rem] h-[9rem] border shadow-sm border-gray-300 rounded-2xl overflow-hidden">
-                <img
-                  src="https://images.pexels.com/photos/23775788/pexels-photo-23775788.jpeg"
-                  alt=""
-                  className=" h-full object-cover"
-                />
-              </div>
-
-              <div className="w-full h-[9rem] border shadow-sm rounded-2xl overflow-hidden">
-                <img
-                  src="https://images.pexels.com/photos/2323080/pexels-photo-2323080.jpeg"
-                  alt=""
-                  className="w-full h-full object-cover"
-                />
-              </div>
+            <div className="w-full h-[9rem] border shadow-sm rounded-2xl overflow-hidden">
+              <img
+                src="https://images.pexels.com/photos/2323080/pexels-photo-2323080.jpeg"
+                alt=""
+                className="w-full h-full object-cover"
+              />
             </div>
           </div>
         </div>
